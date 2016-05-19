3d1978a88ef239f8f326cc4885ba5a3442a6b012
march-dave <ysangy@gmail.com>
2016-05-18 17:04:58 -0700

    mogoose add

--- app.js
+++ app.js
@@ -11,8 +11,15 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var pbalbum = require('./routes/pbalbum');
 
 var app = express();
+
+var mongoose = require('mongoose');
+const MONGOURL = process.env.MONGODB_URI || 'mongodb://localhost:/pba-app'
+mongoose.connect(MONGOURL, err => {
+    console.log(err || `Connected to MongoDB at ${MONGOURL}`);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/pbalbum', pbalbum);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
